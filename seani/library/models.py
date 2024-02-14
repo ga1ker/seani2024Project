fdfbdb34b78b8dced4d75fb93ef17c2c83a513e4
ga1ker <2019gadiel.alcazar@cobatlaxcala.edu.mx>
2024-02-14 09:54:10 -0600

    02 App Library Model Module and Question

---
+++ seani/library/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Module(models.Model):
+    name =models.CharField(
+        verbose_name= 'Nombre',
+        max_length=100)
+    description = models.TextField(
+        verbose_name='Descripcion',
+        max_length= 200)
+    
+    def _str_(self):
+        return self.name
+    
+    class Meta: 
+        verbose_name = 'Modulo'
+        verbose_name_plural = 'Modulos'
+
+
+class Question(models.Model):
+    module=models.ForeignKey(Module, on_delete=models.CASCADE, verbose_name = 'Modulo')
+    question_text = models.CharField(
+        verbose_name="Texto de la pregunta",
+        max_length=200,blank=True,
+        null = True)
+    question_image= models.ImageField(
+        verbose_name="Imagen de la pregunta",
+        
+        upload_to='Question',blank=True,
+        null=True)
+    answer1= models.CharField(max_length=200, verbose_name="Respuesta A")
+    answer2= models.CharField(max_length=200, verbose_name="Respuesta B")
+    answer3= models.CharField(max_length=200, verbose_name="Respuesta C", null = True, blank = True)
+    answer4= models.CharField(max_length=200, verbose_name="Respuesta D", null = True, blank = True)
+    correct= models.CharField(max_length=5, verbose_name="Respuesta Correcta")
+
+    
+    def _str_(self):
+        return f"{self.module}-{self.id}"
+    class Meta: 
+        verbose_name = 'Pregunta'
+        verbose_name_plural = 'Preguntas'
--- seani/seani/settings.py
+++ seani/seani/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #local apps
+    'library.apps.LibraryConfig',
+
+    #django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
